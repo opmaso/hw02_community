45aa6bfa867b8492288a14adfe4618786ab5d639
Антонюк Никита <nikitosym@gmail.com>
2022-09-11 21:32:36 +0300

    rr

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from .models import Post, Group
 
 
-
 class PostAdmin(admin.ModelAdmin):
-   
+
     list_display = (
         'pk',
         'text',
@@ -12,21 +11,18 @@ class PostAdmin(admin.ModelAdmin):
         'author',
         'group'
     )
-   
+
     list_editable = ('group',)
-    
+
     search_fields = ('text',)
-    
+
     list_filter = ('pub_date',)
-   
+
     empty_value_display = '-пусто-'
 
 
-
 class GroupAdmin(admin.ModelAdmin):
-    
     prepopulated_fields = {'slug': ('title',)}
-
 
 
 admin.site.register(Post, PostAdmin)
